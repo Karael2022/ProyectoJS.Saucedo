a6d98193c09d794625eb8a73ae5a80551b20f98b
Roxana Saucedo <Kara.el2023@gmail.com>
2022-06-07 00:35:17 -0300

    Desafio 2

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-let sunombre = prompt ("Ingrese su nombre")
-alert("Bienvenido " + sunombre)
-alert("Ahora quiero conocer tu edad")
-let edad = prompt ("Ingresa tu edad")
-alert("Para salir escribe ESC")
-alert("vamos a multiplicar y a dividir entonces! " + sunombre)
 
-for (let operacion = 0; operacion < 3; operacion++) {
-    
-    if(operacion == 1){
-        let num1 = prompt("Vamos a multiplicar: Ingrese un numero");
-        let num2 = prompt("Ingrese el segundo numero a multiplicar");
-        let result = (parseInt(num1) * parseInt(num2));
-        alert("El resultado es: " + result);
-    }
+const precio_Producto1 = 100;
+const precio_Producto2 = 200;
+const precio_Producto3 = 150;
+const precio_Producto4 = 250;  
 
-    else (operacion == 2)
-        let num1 = prompt("Vamos a dividir: Ingrese un numero");
-        let num2 = prompt("Ingrese el segundo numero a dividir");
-        let result = (parseInt(num1) / parseInt(num2));
-alert("El resultado es: " + result);
+let nombre = prompt("Vamos al super!! Ingresa tu nombre")
+let Producto1 = prompt("Ingresa 1er producto para comprar " +nombre);
+let Producto2 = prompt("Ingresa 2do producto para comprar");
+let Producto3 = prompt("Ingresa 3er producto para comprar");
+let Producto4 = prompt("Ingresa 4to producto para comprar");
+if ((Producto1 != "") && (Producto2 != "") && (Producto3 != "") && (Producto4 != "")) 
+{ let carrito = "1) "+ Producto1 + " " + 
+                "2) "+ Producto2 + " " +
+                "3) "+ Producto3 + " " +
+                "4) "+ Producto4;
+                alert(carrito);
 }
+else {
+    alert ("Se esta olvidando algun producto");
+}
+let descuento20 = precio_Producto1 - (precio_Producto1 * 0,2);
+let descuento30 = precio_Producto2 - (precio_Producto2 * 0,3);
+alert("Ganaste un 20% descuento en el 1er producto Total $" +descuento20);
+alert("Te haremos un 30% de descuento en el 2do producto Total $" +descuento30);
 
-alert("Ha sido un gusto! si necesitas continuar pulsa F5")
-
+let Subtotal_productos = descuento20 + descuento30 + precio_Producto3 + precio_Producto4;
+alert ("Total a pagar" + " $ "+Subtotal_productos);
+alert ("Te esperamos pronto!")
---
+++ index3.js
@@ -0,0 +1,151 @@
+
+/*let Subtotal_bebidas=0;
+let Subtotal_Salsa=0;
+let Subtotal_caja=0;
+let Total=0;
+let Pedido="";
+
+const Precio_Bebida_1=200;
+const Precio_Bebida_2=250;
+const Precio_Salsa_1=100;
+const Precio_Salsa_2=100;
+const Precio_Salsa_3=100;
+const Precio_caja_1=500;
+const Precio_caja_2=600;
+const Precio_caja_3=700;
+
+
+const Bebida_1="Coca Cola"
+const Bebida_2="Coca Zero"
+const Bebida_3="Agua Mineral"
+const Salsa_1="Salsa barbacoa"
+const Salsa_2="Salsa Picante"
+const Salsa_3="Salsa Mostaza y miel"
+const Caja_1="Cuarto trasero"
+const Caja_2="12 Piezas de pechugas"
+const Caja_3="12 alitas de pollo" */ 
+
+
+/*function operacion(valor1,valor2,operacion) {
+    switch (operacion) {
+        case 1:
+            return valor1 + valor2;
+            break;
+        case 2:
+            return valor1 - valor2;
+            break;
+        case 3:
+            return valor1 * valor2;
+            break;
+        case 3:
+            return valor1 / valor2;
+        default:
+            break;
+    }
+}*/
+
+/*let nombre_completo = "Bart Simpson";
+let direccion = "742 Av. Siempreviva";
+let ciudad = "Sprinfield";
+let pais = "USA"
+let cumpleanos ="2-11-70"
+let sexo = "masculino"
+
+let carnet = "Nombre: "+nombre_completo+" "+
+            "direccion: "+direccion+" "+
+            "ciudad : "+ciudad+" "+
+            "pais: "+pais+" "+
+            "cumpleanos: "+cumpleanos+" "+
+            "sexo: "+sexo;
+alert(carnet);*/    
+
+
+/*let integrante_1 = prompt("Ingresar 1er integrante");
+let integrante_2 = prompt("Ingresar 2do integrante");
+let integrante_3 = prompt("Ingresar 3er integrante");
+let integrante_4 = prompt("Ingresar 4to integrante");
+let integrante_5 = prompt("Ingresar 5to integrante");
+
+let familia = "1) "+integrante_1+" "+
+            "2) "+integrante_2+" "+
+            "3) "+integrante_3+" "+
+            "4) "+integrante_4+" "+
+            "5) "+integrante_5;
+    alert(familia);*/
+
+
+
+/*let Precio_Bebida_1 = parseInt(prompt("Ingresar precio de una 1ra bebida "));
+let Precio_Bebida_2 = parseInt(prompt("Ingresar precio de una 2da bebida "));
+let descuento20 = Precio_Bebida_1 - (Precio_Bebida_1 * 0,2);
+let descuento30 = Precio_Bebida_2 - (Precio_Bebida_2 * 0,3);
+alert("Ganaste un 20% descuento en la 1er bebida Total $" +descuento20);
+alert("Te haremos un 30% de descuento en la 2da bebida Total $" +descuento30);*/
+
+
+/*let entrada = prompt("Ingrese un nombre");
+let Ingresar_nombre = "Felpita";
+if (entrada == Ingresar_nombre) {
+    alert("Fui yo");}
+    else {
+        ("Yo no fui");
+}*/
+
+/*let entrada = prompt("Ingrese una tecla");
+switch (entrada) {
+    case "y":
+        alert("Correto");
+        break;
+    case "Y":
+        alert("Correcto");
+    break;
+    default:
+        alert("Incorrecto")
+        break;
+}*/
+
+/*let entrada3 = prompt("INGRESAR UN NUMERO");
+if (entrada3 == 1) {
+    alert("ELEGISTE A HOMERO");
+}else if (entrada3 == 2) {
+    alert("ELEGISTE A MARGE");
+}else if (entrada3 == 3) {
+    alert("ELEGISTE A BART");
+}else if (entrada3 == 4) {
+    alert("ELEGISTE A LISA");
+}
+else {
+    alert("ERROR"); 
+}*/
+
+/*let entrada1 = prompt("Ingrese un valor de venta para su auto");
+if ((entrada1 >= 0) && (entrada1 <= 1000)) {
+    alert("Barato! Lo compro!");
+}
+    else if 
+        ((entrada1 > 1001) && (entrada1 <= 3000)) {
+            alert("MMmm precio aceptable");
+    }
+    else if (entrada1 > 3001)  {
+        alert("Muy caro, paso!");
+    }
+    else {
+        alert("Muchas gracias");
+    }*/
+
+
+/*let Producto1 = prompt("Ingrese 1er producto para comprar");
+let Producto2 = prompt("Ingrese 2do producto para comprar");
+let Producto3 = prompt("Ingrese 3er producto para comprar");
+let Producto4 = prompt("Ingrese 4to producto para comprar");
+if ((Producto1 != "") && (Producto2 != "") && (Producto3 != "") && (Producto4 != "")) 
+{ let carrito = "1) "+ Producto1 + " " +
+                "2) "+ Producto2 + " " +
+                "3) "+ Producto3 + " " +
+                "4) "+ Producto4;
+                alert(carrito);
+}
+else {
+    alert ("Se esta olvidando algun producto");
+}
+*/
