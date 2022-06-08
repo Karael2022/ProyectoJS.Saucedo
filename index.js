4a9b1b204f0cac53daa2a07726759df510e10a2d
Roxana Saucedo <Kara.el2023@gmail.com>
2022-06-08 08:11:32 -0300

    Desafia 2 v2

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 
-const precio_Producto1 = 100;
-const precio_Producto2 = 200;
-const precio_Producto3 = 150;
-const precio_Producto4 = 250;  
+let producto;
+let Pedido;
+let Subtotal_bebidas;
+let Subtotal_Salsa;
+let Subtotal_caja;
+let Subtotal_producto;
+let Total;
 
-let nombre = prompt("Vamos al super!! Ingresa tu nombre")
-let Producto1 = prompt("Ingresa 1er producto para comprar " +nombre);
+
+
+//Funciona pero no continua 
+function PedirNombre() {
+    let nombre = prompt("Vamos al super!! Ingresa tu nombre");
+    while (nombre === "" || !isNaN(nombre)) {
+        nombre = ("Ingrese su nombre");
+        
+    }
+}
+PedirNombre();
+
+/*let nombre = prompt("Vamos al super!! Ingresa tu nombre");
+    while (nombre === "" || !isNaN(nombre)) {
+        nombre = ("Ingrese su nombre");
+    }*/
+    
+/*function saludar() {
+    alert("Hola " + nombre)
+    
+}
+saludar()*/
+
+const pedido = () => {
+    producto = prompt("Que caja desea llevar? banana , manzana, pera?");
+
+    let precio;
+    switch (producto) {
+        case "banana":
+            precio = 500;
+            break;
+        case "manzana":
+            precio = 600;
+            break;
+        case "pera":
+            precio = 800;
+            break;
+        default:
+            alert("Aun no ingreso caja valida");
+            break;
+    }
+        return precio;
+}
+
+
+
+/*let Producto1 = prompt("Ingresa 1er producto para comprar " + nombre);
 let Producto2 = prompt("Ingresa 2do producto para comprar");
 let Producto3 = prompt("Ingresa 3er producto para comprar");
 let Producto4 = prompt("Ingresa 4to producto para comprar");
@@ -18,7 +66,9 @@ if ((Producto1 != "") && (Producto2 != "") && (Producto3 != "") && (Producto4 !=
 }
 else {
     alert ("Se esta olvidando algun producto");
-}
+}*/
+
+
 let descuento20 = precio_Producto1 - (precio_Producto1 * 0,2);
 let descuento30 = precio_Producto2 - (precio_Producto2 * 0,3);
 alert("Ganaste un 20% descuento en el 1er producto Total $" +descuento20);
