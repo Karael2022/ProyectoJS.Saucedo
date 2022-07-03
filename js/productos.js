192d02b3c13078b980099d09b9813a0567dae88b
Roxana Saucedo <Kara.el2023@gmail.com>
2022-07-03 02:48:31 -0300

    seguimos con Css

--- js/productos.js
+++ js/productos.js
@@ -4,27 +4,27 @@ const productos = [
     {id:1, 
     nombre:"Xiaomi Band 6", 
     precio: "5.123",
-    imagen:"https://stylewatch.vtexassets.com/arquivos/ids/174852-800-auto?width=800&height=auto&aspect=true",
+    img:"https://stylewatch.vtexassets.com/arquivos/ids/174852-800-auto?width=800&height=auto&aspect=true",
     
 },
 {
     id:2, 
     nombre:"Smartwatch Colmi Sky 5", 
     precio: "14.795",
-    imagen:"https://stylewatch.vtexassets.com/arquivos/ids/214078-800-auto?width=800&height=auto&aspect=true", 
+    img:"https://stylewatch.vtexassets.com/arquivos/ids/214078-800-auto?width=800&height=auto&aspect=true", 
     
 },
     {id:3,
     nombre:"Smartwactch Garmin Rose", 
     precio: "234.295",
-    imagen:"https://stylewatch.vtexassets.com/arquivos/ids/214962-800-auto?width=800&height=auto&aspect=true", 
+    img:"https://stylewatch.vtexassets.com/arquivos/ids/214962-800-auto?width=800&height=auto&aspect=true", 
     
 },
 {
     id:4,
     nombre:"Smartwatch Colmi P28 Plus", 
     precio: "15.095",
-    imagen:"https://stylewatch.vtexassets.com/arquivos/ids/209935-800-auto?width=800&height=auto&aspect=true",
+    img:"https://stylewatch.vtexassets.com/arquivos/ids/209935-800-auto?width=800&height=auto&aspect=true",
     
 },]
 
